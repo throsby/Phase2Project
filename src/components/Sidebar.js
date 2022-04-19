825fa4eb0cc64cf7bf3f097b6fb8ad09cc1ea54e
itsagift <anna.c.reynolds@gmail.com>
2022-04-19 16:52:59 -0400

    Added sidebar dropdown

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,23 @@
 import Restaurants from "./Restaurants"
 import Add from "./Add"
 import Search from "./Search"
+import { useState } from "react";
 
-export default function Sidebar(){
+export default function Sidebar({restaurantArray}){
+  const uniqueCuisines = [...new Set(restaurantArray.map(item => item.cuisine_description))];
+  const [selectedCuisine, setSelectedCuisine] = useState("");
+  
 return(
   <div className="sidebar">
-    <select name="cuisine">
-      
+    <select name="cuisine" onChange={e => setSelectedCuisine(e.target.value)}>
+      <option value="">Select cuisine</option>
+      {uniqueCuisines.map((element) => {
+        if (element){
+          return(
+            <option value={element}>{element}</option> 
+          )
+        }
+      })}
     </select>
     <Restaurants/>
     <Add/>
