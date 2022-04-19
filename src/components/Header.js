a61249ed99244b3a9e4cc8d8c860e4c0e680a510
Throsby Wells <throsbywells@gmail.com>
2022-04-19 16:38:54 -0400

    added spin

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
 export default function Header(){
 
     return(<div className="header">
-        <h1>Welcome to Potato Search 🥔</h1>
+        <div className="tato"><h1>Welcome to Potato Search </h1><h1 className="potato">🥔</h1></div>
         <h2>Your home for potatos and all potato related content</h2>
-
     </div>)
 }
