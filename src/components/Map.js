187eed17968533e7344d947a5e767101e9e23d44
Throsby Wells <throsbywells@gmail.com>
2022-04-19 14:21:00 -0400

    commit with Map changes

---
+++ src/components/Map.js
@@ -0,0 +1,32 @@
+// import React, { useRef, useEffect, useState } from 'react';
+
+// import mapboxgl from 'mapbox-gl';
+
+// mapboxgl.accessToken = "pk.eyJ1IjoidGhyb3NieXciLCJhIjoiY2wxZ3locXlnMDJvZzNjczYwaGV0djdsbiJ9.nvGIb4KYulk2zFd-ysHfwA"
+// function Map(){
+// const mapContainer = useRef(null);
+// const map = useRef(null);
+// const [lng, setLng] = useState(-70.9);
+// const [lat, setLat] = useState(42.35);
+// const [zoom, setZoom] = useState(9);
+
+// useEffect(() => {
+//     if (map.current) return; // initialize map only once
+//     map.current = new mapboxgl.Map({
+//     container: mapContainer.current,
+//     style: 'mapbox://styles/mapbox/streets-v11',
+//     center: [lng, lat],
+//     zoom: zoom
+//     });
+//     });
+
+// return (
+//     <div>
+//     <div ref={mapContainer} className="map-container" />
+//     </div>
+//     );
+// }
+
+export default function Map(){
+
+}
