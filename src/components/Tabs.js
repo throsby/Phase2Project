e1bb80a845106b12d043938f79fee07114612a5a
itsagift <anna.c.reynolds@gmail.com>
2022-04-20 15:09:29 -0400

    Tabs rework

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,27 +1,39 @@
 import react from "react"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import Tab from "./Tab"
-import { Route, Switch, NavLink } from "react-router-dom";
+import { Route, Switch, NavLink, useHistory, useParams } from "react-router-dom";
 
 export default function Tabs(){
 const [tabOne, setTabOne] = useState([]);
 const [tabTwo, setTabTwo] = useState([]);
 const [tabThree, setTabThree] = useState([]);
 
+
+const tabs = [
+  {name: "one",
+    content: tabOne
+  },
+  {
+    name: "two",
+    content: tabTwo
+  },
+  {
+    name: "three",
+    content: tabThree
+  }
+]
+
+
+
 return(
   <div className="tabs">
-    <NavLink
-      to="/1"
-      className={({ isActive }) => (isActive ? 'active' : 'navlink')}
-    >Tab 1</NavLink>
-     <NavLink
-      to="/2"
-      className={({ isActive }) => (isActive ? 'active' : 'navlink')}
-    >Tab 2</NavLink>
-    <NavLink
-      to="/3"
-      className={({ isActive }) => (isActive ? 'active' : 'navlink')}
-    >Tab 3</NavLink>
+    {
+      tabs.map((tab) => (
+    <button>
+        {tab.name}
+    </button>
+      ))
+    }
     <Switch>
     <Route exact path="/">
         <Tab tabContent={tabOne}/>
