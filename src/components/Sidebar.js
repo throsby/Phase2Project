181f733e6092ad44d5afe437e1d083742d7382a1
itsagift <anna.c.reynolds@gmail.com>
2022-04-20 09:42:45 -0400

    fixed sidebar props AGAIN AGAIN

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,23 @@
 import Restaurants from "./Restaurants"
 import Add from "./Add"
-import Search from "./Search"
+
 import { useState } from "react";
 
-export default function Sidebar({restaurantArray}){
+export default function Sidebar({restaurantArray, setSelectedCuisine, selectedCuisine}){
   const uniqueCuisines = [...new Set(restaurantArray.map(item => item.cuisine_description))];
-  const [selectedCuisine, setSelectedCuisine] = useState("");
+  
   
 return(
   <div className="sidebar">
     <select name="cuisine" onChange={e => setSelectedCuisine(e.target.value)}>
       <option value="">Select cuisine</option>
       {uniqueCuisines.map((element) => {
-        if (element){
+        if (!element){
           return(
-            <option value={element}>{element}</option> 
+            undefined
           )
         }
+        return <option value={element}>{element}</option> 
       })}
     </select>
     <Restaurants/>
