2bd40ec11e1e89b4f00e30b4fa8c51c8b31f4168
itsagift <anna.c.reynolds@gmail.com>
2022-04-20 11:02:20 -0400

    Created a mockup UI without dynamic variables

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,3 +1,37 @@
 export default function Restaurants(){
+  function capitalize (string){
+    return string.toLowerCase().split(" ").map(element => {
+      return element[0].toUpperCase() + element.slice(1);
+    }).join(" ")
+  }
+  const testArray = [
+    {
+      name: "HILLSTONE MANHATTAN",
+      street: "PARK AVENUE SOUTH",
+      building: "378",
+      borough: "Manhattan"
+    },
+    {
+      name: "DOMINO'S",
+      street: "WESTCHESTER AVENUE",
+      building: "2025",
+      borough: "Bronx"
+    }
+  ]
+  return(
+  <div>
+    {
+      testArray.map((element)=> {
+        return(
+          <div>
+            <h4 className="restaurant-title">{element.name}</h4>
+            <p className="restaurant-borough">{element.borough}</p>
+            <p className="restaurant-address">{`${element.building} ${capitalize(element.street)}`}</p>
+          </div>
+        )
+      })
+    }
+  </div>
+  )
   
 }
