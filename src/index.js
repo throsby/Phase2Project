56ac76604a3aea34ae9d3ac30408e0306c6abade
Throsby Wells <git@throsby.dev>
2024-02-06 20:25:01 -0500

    got started adding datadog

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+// import { datadogRum } from '@datadog/browser-rum'
 
+// datadogRum.init({
+//   applicationId: 'HotsPot',
+//   clientToken: 'pub859f5a4d362390c42f3fc333429577d5',
+//   site: 'us5.datadoghq.com',
+//   service: 'hotspot',
+//   env: 'production',
+//   version: '1.0.0',
+//   sessionSampleRate: 100,
+//   sessionReplaySampleRate: 100,
+//   trackResources: true,
+//   trackLongTasks: true,
+//   trackUserInteractions: true,
+// });
+
+// console.log("Datadog RUM intialized")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
